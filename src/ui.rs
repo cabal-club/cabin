3daa880b4295c2265382d7be0bbc8bc9f5de873b
substack <substack@bits.coop>
2022-03-05 09:11:19 -0500

    timestamp

--- src/ui.rs
+++ src/ui.rs
@@ -9,6 +9,7 @@ pub struct UI {
   active_window: usize,
   windows: Vec<Window>,
   diff: ansi_diff::Diff,
+  size: TermSize,
 }
 
 impl UI {
@@ -16,6 +17,7 @@ impl UI {
     let mut windows = vec![Window::new(vec![], "!status".as_bytes().to_vec())];
     Self {
       diff: ansi_diff::Diff::new(size),
+      size,
       active_window: 0,
       windows,
     }
@@ -25,22 +27,31 @@ impl UI {
   }
   pub fn write_status(&mut self, msg: &str) {
     self.windows.get_mut(0).unwrap().write_ln(msg);
+    self.update();
   }
   pub fn get_active(&mut self) -> (Addr,Channel) {
     let w = self.windows.get(self.active_window).unwrap();
     (w.addr.clone(), w.channel.clone())
   }
-  pub fn render(&mut self) {
+  pub fn update(&mut self) {
+    let w = self.windows.get(self.active_window).unwrap();
+    let update = w.lines.iter().map(|(time,line)| {
+      format!["[{}] {}", timestamp(*time), line].to_string()
+    }).collect::<Vec<String>>().join("\n");
+    print!["{}", self.diff.update(&update)];
   }
+}
+
+fn timestamp(time: u64) -> String {
+  time_format::strftime_utc("%H:%M", time as time_format::TimeStamp).unwrap()
 }
 
 pub struct Window {
   addr: Addr,
   channel: Channel,
-  time_start: u64,
   time_end: u64,
   limit: usize,
-  lines: Vec<String>,
+  lines: Vec<(u64,String)>,
 }
 
 impl Window {
@@ -48,14 +59,14 @@ impl Window {
     Self {
       addr,
       channel,
-      time_start: now() - 15*60,
+      //time_start: now() - 15*60,
       time_end: 0,
       limit: 50,
       lines: vec![],
     }
   }
   pub fn write_ln(&mut self, msg: &str) {
-    self.lines.extend(msg.split('\n').map(|s| s.to_string()));
+    self.lines.push((now(), msg.to_string()));
   }
 }
 
